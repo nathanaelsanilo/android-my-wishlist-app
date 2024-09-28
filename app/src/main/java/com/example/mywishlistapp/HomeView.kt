4fde366a847e938e3704de6e36a37469b23464cc
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-09-28 10:09:08 +0700

    feat: add fab

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,13 +1,20 @@
 package com.example.mywishlistapp
 
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 // Scaffold: structure of the apps (layout)
 @Composable
@@ -20,7 +27,11 @@ fun HomeView() {
             })
         },
 
-        ) {
+        floatingActionButton = {
+            FabAdd(context)
+        }
+
+    ) {
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -28,5 +39,19 @@ fun HomeView() {
         ) {
 
         }
+    }
+}
+
+@Composable
+fun FabAdd(context: Context) {
+    FloatingActionButton(
+        onClick = {
+            Toast.makeText(context, "FAB Works!", Toast.LENGTH_LONG).show()
+        },
+        modifier = Modifier.padding(all = 20.dp),
+        contentColor = Color.White,
+        backgroundColor = Color.Black,
+    ) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "Add button")
     }
 }
