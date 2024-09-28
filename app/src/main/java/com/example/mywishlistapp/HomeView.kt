f2be41127700e8d7407b18ac6fd03490c69ac775
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-09-28 10:22:15 +0700

    feat: add wish item compose

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -2,19 +2,26 @@ package com.example.mywishlistapp
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.mywishlistapp.data.Wish
 
 // Scaffold: structure of the apps (layout)
 @Composable
@@ -53,5 +60,29 @@ fun FabAdd(context: Context) {
         backgroundColor = Color.Black,
     ) {
         Icon(imageVector = Icons.Default.Add, contentDescription = "Add button")
+    }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+            .clickable { onClick() },
+        elevation = 10.dp,
+        backgroundColor = Color.White
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = wish.title,
+                fontWeight = FontWeight.ExtraBold
+            )
+            Text(
+                text = wish.description,
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -0,0 +1,7 @@
+package com.example.mywishlistapp.data
+
+data class Wish(
+    val id : Long = 0L,
+    val title : String = "",
+    val description : String = ""
+)
