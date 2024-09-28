689ccc02e97337dcf9611e044812ff6f3cac759a
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-09-28 09:46:24 +0700

    feat: add navigation back button

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -2,10 +2,15 @@ package com.example.mywishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +19,17 @@ fun AppBarView(
     title: String,
     onBackNavClick: () -> Unit = {}
 ) {
+
+    val navigationIcon: (@Composable () -> Unit)? = {
+        IconButton(onClick = { onBackNavClick() }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
+    }
+
     TopAppBar(
         title = {
             Text(
@@ -26,5 +42,6 @@ fun AppBarView(
         },
         elevation = 3.dp,
         backgroundColor = colorResource(R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 }
--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,22 +1,31 @@
 package com.example.mywishlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 // Scaffold: structure of the apps (layout)
 @Composable
 fun HomeView() {
-
+    val context = LocalContext.current
     Scaffold(
-        topBar = { AppBarView("Wishlist") }
-    ) {
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
+        topBar = {
+            AppBarView("Wishlist", {
+                Toast.makeText(context, "Back Navigation Works!", Toast.LENGTH_LONG).show()
+            })
+        },
+
+        ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
 
         }
     }
