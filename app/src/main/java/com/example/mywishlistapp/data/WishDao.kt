ee9730453203a91eec99e759a481e71980b68da3
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-10-05 09:24:03 +0700

    feat(db): setup dao

--- app/src/main/java/com/example/mywishlistapp/data/WishDao.kt
+++ app/src/main/java/com/example/mywishlistapp/data/WishDao.kt
@@ -1,4 +1,30 @@
 package com.example.mywishlistapp.data
 
-class WishDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class WishDao {
+    // abstract class doesnt have implementation
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun addWish(wishEntity: Wish)
+
+    @Query("select * from `wish-table`")
+    // Flow -> part of kotlin coroutine to handle async data streams in a reactive way
+    abstract fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateWish(entity: Wish)
+
+    @Delete
+    abstract suspend fun deleteWish(entity: Wish)
+
+    @Query("select * from `wish-table` where id=:id")
+    abstract fun getWish(id: Long): Flow<Wish>
 }
