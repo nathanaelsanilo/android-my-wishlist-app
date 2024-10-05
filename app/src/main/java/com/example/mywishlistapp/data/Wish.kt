43f5150006cfe3e8110f834add2f7c11463007b9
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-10-05 09:22:27 +0700

    feat: setup wish entity

--- app/src/main/java/com/example/mywishlistapp/data/Wish.kt
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -1,8 +1,18 @@
 package com.example.mywishlistapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table")
 data class Wish(
+    @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+
+    @ColumnInfo("wish-title")
     val title: String = "",
+
+    @ColumnInfo("wish-desc")
     val description: String = ""
 )
 
