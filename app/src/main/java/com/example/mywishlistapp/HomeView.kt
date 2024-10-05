d27529c913b59d2893aee71325ab1eb13b30bcd7
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-10-05 11:59:50 +0700

    feat(delete wish): show background when swipe

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,6 +1,9 @@
 package com.example.mywishlistapp
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,9 +22,12 @@ import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -70,8 +76,25 @@ fun HomeView(navController: NavController, viewModel: WishViewModel) {
                 // Composable swipe to dismiss
                 SwipeToDismiss(
                     state = dismissState,
-                    background = {},
+                    background = {
+                        val color: Color =
+                            if (dismissState.dismissDirection == DismissDirection.EndToStart) {
+                                Color.Red
+                            } else Color.Transparent
+
+                        val bgColor by animateColorAsState(targetValue = color, label = "")
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(bgColor)
+                                .padding(horizontal = 20.dp), contentAlignment = Alignment.CenterEnd
+                        ) {
+                            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete", tint = Color.White)
+                        }
+                    },
                     directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
+                    // the swipe thresholds that will trigger the dismiss state change
                     dismissThresholds = { FractionalThreshold(0.25f) },
                 ) {
                     WishItem(wish = wishItem) {
