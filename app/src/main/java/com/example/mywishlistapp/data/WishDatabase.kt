29ec1f86412f4e6efca63c3b4ff4f10d4e389936
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-10-05 09:24:40 +0700

    feat(db): setup database

--- app/src/main/java/com/example/mywishlistapp/data/WishDatabase.kt
+++ app/src/main/java/com/example/mywishlistapp/data/WishDatabase.kt
@@ -1,4 +1,11 @@
 package com.example.mywishlistapp.data
 
-class WishDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Wish::class], version = 1, exportSchema = false)
+abstract class WishDatabase : RoomDatabase() {
+
+    // give access to wish dao
+    abstract fun wishDao(): WishDao
 }
